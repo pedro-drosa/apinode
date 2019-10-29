9db8087f152463f53fa5ee71b356aaa8e7b101c2
Pedro Juraci <pedro_drosa@hotmail.com>
2019-10-29 18:19:40 -0300

    created: authentication route

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
+const bcryptjs =require('bcryptjs');
 module.exports = {
-    async store(req, res){
+    async register(req, res){
         try {
             const {email} = req.body;
             if (await User.findOne({email})) {
@@ -13,5 +14,17 @@ module.exports = {
         } catch (error) {
             return res.status(400).send({error:'Registration Failed'});
         }
+    },
+    async authenticate(req, res){
+        const {email, password} = req.body;
+        const user = await User.findOne({email}).select('+password');
+        if (!user) {
+            return res.status(400).send({error:'user not found'});
+        }
+        if(!await bcryptjs.compare(password, user.password)){
+            return res.status(400).send({error:'Invalid Password'});
+        }
+        user.password = undefined;
+        res.send({user});
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const {Router} = require('express');
 const authController = require('./controllers/authController');
 const routes = Router();
 
-routes.post('/register',authController.store);
+routes.post('/register',authController.register);
+routes.post('/authenticate',authController.authenticate);
 
 module.exports = routes;
