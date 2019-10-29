2235cccdd7604fc1c769f5e92e2f3014b7eb4979
Pedro Juraci <pedro_drosa@hotmail.com>
2019-10-29 17:16:45 -0300

    some adjustments

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,6 +7,8 @@ module.exports = {
                 return res.status(400).send({error:"user alread exists"});
             }
             const user = await User.create(req.body);
+            user.password = undefined;
+            
             return res.json(user);
         } catch (error) {
             return res.status(400).send({error:'Registration Failed'});
